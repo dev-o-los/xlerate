38e2d2fa5066f6b64c57f458b0973bbf09218398
devolos <utk27032005@gmail.com>
2023-06-30 19:56:36 +0530

    all fields added for reactive home + exported

--- lib/simple_sizer.dart
+++ lib/simple_sizer.dart
@@ -2,3 +2,4 @@ library simple_sizer;
 
 export 'src/size_config.dart';
 export 'src/reactive_row_col.dart';
+export 'src/reactive_home.dart';
--- lib/src/reactive_home.dart
+++ lib/src/reactive_home.dart
@@ -1,44 +1,68 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({
+class ReactiveHome extends StatefulWidget {
+  const ReactiveHome({
     super.key,
-    required this.list,
+    required this.pages,
+    required this.bottomNavBarDestinations,
     this.bottomNavBarHeight,
+    this.labelBehavior,
+    this.animationDuration,
+    this.backgroundColor,
+    this.elevation,
+    this.indicatorColor,
+    this.indicatorShape,
+    this.shadowColor,
   });
 
-  final List<Widget> list;
+  final List<Widget> pages;
+  final List<NavigationDestination> bottomNavBarDestinations;
+
   final double? bottomNavBarHeight;
+  final double? elevation;
+
+  final Duration? animationDuration;
+
+  final Color? backgroundColor;
+  final Color? indicatorColor;
+  final Color? shadowColor;
+
+  final NavigationDestinationLabelBehavior? labelBehavior;
+
+  final ShapeBorder? indicatorShape;
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  State<ReactiveHome> createState() => _ReactiveHomeState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _ReactiveHomeState extends State<ReactiveHome> {
+  int _currentindex = 0;
+
+  void _changePage(int index) {
+    setState(() {
+      _currentindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: widget.list[0],
+      body: widget.pages[_currentindex],
       bottomNavigationBar: NavigationBar(
-        //selectedIndex: con.currentindex,
+        selectedIndex: _currentindex,
         height: widget.bottomNavBarHeight,
-        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.home), label: 'Search'),
-          NavigationDestination(icon: Icon(Icons.home), label: 'Downloads'),
-        ],
+        labelBehavior: widget.labelBehavior,
+        animationDuration: widget.animationDuration,
+        backgroundColor: widget.backgroundColor,
+        elevation: widget.elevation,
+        indicatorColor: widget.indicatorColor,
+        indicatorShape: widget.indicatorShape,
+        shadowColor: widget.shadowColor,
+        key: widget.key,
+        destinations: widget.bottomNavBarDestinations,
         surfaceTintColor: Colors.transparent,
-        //onDestinationSelected: (value) {},
+        onDestinationSelected: (value) => _changePage(value),
       ),
     );
   }
 }
-// class ScreenController extends GetxController {
-//   int currentindex = 0;
-
-//   void changePage(int index) {
-//     currentindex = index;
-//     update(['page']);
-//   }
-// }
