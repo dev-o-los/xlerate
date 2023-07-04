869c533c2ebb0b71f24a9aa618854b6c09be0571
devolos <utk27032005@gmail.com>
2023-07-04 18:21:46 +0530

    exports added

--- lib/simple_sizer.dart
+++ lib/simple_sizer.dart
@@ -1,7 +1,16 @@
 library simple_sizer;
 
+//From Folder exports
 export 'src/exports/size_config.dart';
 export 'src/exports/reactive_row_col.dart';
 export 'src/exports/reactive_home.dart';
+
+//From Folder extensions
 export 'src/extensions/build_ctx.dart';
+export 'src/extensions/align_ext.dart';
+export 'src/extensions/expanded_and_flexible_ext.dart';
+export 'src/extensions/fractionally_box.dart';
+export 'src/extensions/padding_ext.dart';
+
+//From Folder reactive_widgets
 export 'src/reactive_widgets/reactive_destination.dart';
