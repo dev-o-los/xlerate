768c7d72e7cd6d46f8949c3cd9ea0f9ca97e1b4e
devolos <utk27032005@gmail.com>
2023-07-10 17:36:26 +0530

    txt ext docs completed + exported

--- lib/simple_sizer.dart
+++ lib/simple_sizer.dart
@@ -4,6 +4,7 @@ library simple_sizer;
 export 'src/exports/size_config.dart';
 export 'src/exports/reactive_row_col.dart';
 export 'src/exports/reactive_home.dart';
+export 'src/exports/reactive_border_rad.dart';
 
 //From Folder extensions
 export 'src/exports/extensions/build_ctx.dart';
@@ -16,6 +17,7 @@ export 'src/exports/extensions/sizedbox_ext.dart';
 export 'src/exports/extensions/fitted_box_ext.dart';
 export 'src/exports/extensions/limitedbox_ext.dart';
 export 'src/exports/extensions/stba_ext.dart';
+export 'src/exports/extensions/text_ext.dart';
 
 //From Folder reactive_widgets
 export 'src/reactive_widgets/reactive_destination.dart';
--- lib/src/exports/extensions/text_ext.dart
+++ lib/src/exports/extensions/text_ext.dart
@@ -1,37 +1,80 @@
 import 'package:flutter/widgets.dart';
 
 extension TextExt on Text {
+  /// The `bold` is an extension method on the `Text` widget in Flutter. It allows you to
+  /// easily create a new `Text` widget with a bold font weight.
   Text get bold =>
       textHelper(data!, style!.copyWith(fontWeight: FontWeight.bold));
 
+  /// The `semibold` is an extension method on the `Text` widget in Flutter. It allows you to
+  /// easily create a new `Text` widget with a semi-bold font weight.
   Text get semibold =>
       textHelper(data!, style!.copyWith(fontWeight: FontWeight.w600));
 
+  /// The function `col` returns a `Text` widget with a specified color.
+  ///
+  /// Args:
+  ///   color (Color): The "color" parameter is of type "Color". It represents the desired color for the
+  /// text.
   Text col(Color color) => textHelper(data!, style!.copyWith(color: color));
 
+  /// The function `bgcol` takes a `Color` parameter and returns a `Text` widget with the specified
+  /// background color.
+  ///
+  /// Args:
+  ///   color (Color): The color parameter is of type Color, which represents a color in Flutter. It can
+  /// be any valid color value, such as a named color from the Colors class or a custom color defined
+  /// using the Color constructor.
   Text bgcol(Color color) =>
       textHelper(data!, style!.copyWith(backgroundColor: color));
 
+  /// The function `fontfm` returns a `Text` widget with a specified font family.
+  ///
+  /// Args:
+  ///   fm (String): The parameter "fm" is a string that represents the font family name.
   Text fontfm(String fm) => textHelper(data!, style!.copyWith(fontFamily: fm));
 
+  /// The function `fntsize` returns a `Text` widget with a specified font size.
+  ///
+  /// Args:
+  ///   size (double): The size parameter is a double value that represents the desired font size for
+  /// the text.
   Text fntsize(double size) =>
       textHelper(data!, style!.copyWith(fontSize: size));
 
+  /// The `itlc` is an extension method on the `Text` widget in Flutter. It allows you
+  /// to easily create a new `Text` widget with an italic font style.
   Text get itlc =>
       textHelper(data!, style!.copyWith(fontStyle: FontStyle.italic));
 
+  /// The `clip` is an extension method on the `Text` widget in Flutter. It allows you
+  /// to easily create a new `Text` widget with the `TextOverflow.clip` property
+  /// set as the overflow behavior.
   Text get clip =>
       textHelper(data!, style!.copyWith(overflow: TextOverflow.clip));
 
+  /// The `ellipsis` is an extension method on the `Text` widget in Flutter. It allows you to
+  /// easily create a new `Text` widget with the `TextOverflow.ellipsis` property set as the overflow
+  /// behavior.
   Text get ellipsis =>
       textHelper(data!, style!.copyWith(overflow: TextOverflow.ellipsis));
 
+  /// The `fade` is an extension method on the `Text` widget in Flutter. It allows you
+  /// to easily create a new `Text` widget with the `TextOverflow.fade` property
+  /// set as the overflow behavior.
   Text get fade =>
       textHelper(data!, style!.copyWith(overflow: TextOverflow.fade));
 
+  /// The `visible` is an extension method on the `Text` widget in Flutter. It allows you to
+  /// easily create a new `Text` widget with the `TextOverflow.visible` property
+  /// set as the overflow behavior.
   Text get visible =>
       textHelper(data!, style!.copyWith(overflow: TextOverflow.visible));
 
+  /// The function `shadows` applies a list of shadows to a text.
+  ///
+  /// Args:
+  ///   sh (List<Shadow>): A list of `Shadow` objects.
   Text shadows(List<Shadow> sh) =>
       textHelper(data!, style!.copyWith(shadows: sh));
 }
