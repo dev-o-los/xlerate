fc8c89de1e8c1819019fef36e9aa0847abb5853c
devolos <utk27032005@gmail.com>
2023-07-10 17:25:58 +0530

    minor structure change

--- lib/simple_sizer.dart
+++ lib/simple_sizer.dart
@@ -6,16 +6,16 @@ export 'src/exports/reactive_row_col.dart';
 export 'src/exports/reactive_home.dart';
 
 //From Folder extensions
-export 'src/extensions/build_ctx.dart';
-export 'src/extensions/align_cntr_ext.dart';
-export 'src/extensions/expanded_and_flexible_ext.dart';
-export 'src/extensions/fractionally_box.dart';
-export 'src/extensions/padding_ext.dart';
-export 'src/extensions/aspectratio_ext.dart';
-export 'src/extensions/sizedbox_ext.dart';
-export 'src/extensions/fitted_box_ext.dart';
-export 'src/extensions/limitedbox_ext.dart';
-export 'src/extensions/stba_ext.dart';
+export 'src/exports/extensions/build_ctx.dart';
+export 'src/exports/extensions/align_cntr_ext.dart';
+export 'src/exports/extensions/expanded_and_flexible_ext.dart';
+export 'src/exports/extensions/fractionally_box.dart';
+export 'src/exports/extensions/padding_ext.dart';
+export 'src/exports/extensions/aspectratio_ext.dart';
+export 'src/exports/extensions/sizedbox_ext.dart';
+export 'src/exports/extensions/fitted_box_ext.dart';
+export 'src/exports/extensions/limitedbox_ext.dart';
+export 'src/exports/extensions/stba_ext.dart';
 
 //From Folder reactive_widgets
 export 'src/reactive_widgets/reactive_destination.dart';
