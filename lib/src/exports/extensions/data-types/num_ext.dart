818c5790de13da092258fb551c563840f8fc09f4
devolos <utk27032005@gmail.com>
2023-08-06 17:37:10 +0530

    Commit Message: 	 - Add getters for converting int to Duration in seconds, minutes, milliseconds, and microseconds.
    Commit Description:
     - This MR adds getters to the codebase that allow converting an integer value to a `Duration` object with the number of seconds, minutes, milliseconds, or microseconds equal to the value of the integer. This provides a convenient way to work with time units in the code.

--- lib/src/exports/extensions/data-types/num_ext.dart
+++ lib/src/exports/extensions/data-types/num_ext.dart
@@ -1,6 +1,17 @@
 extension NumExt on int {
+  /// This getter returns a `Duration` object with the number of seconds equal to the
+  /// value of the `int` on which the getter is called.
   Duration get s => Duration(seconds: this);
+
+  /// This getter returns a `Duration` object with the number of minutes equal to the
+  /// value of the `int` on which the getter is called.
   Duration get min => Duration(minutes: this);
+
+  /// This getter returns a `Duration` object with the number of milliseconds equal to the
+  /// value of the `int` on which the getter is called.
   Duration get milli => Duration(milliseconds: this);
+
+  /// This getter returns a `Duration` object with the number of microseconds equal to the
+  /// value of the `int` on which the getter is called.
   Duration get micro => Duration(microseconds: this);
 }
