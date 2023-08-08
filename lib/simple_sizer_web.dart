10ac6135d4f7fbb47cde3390ca7786f33df29928
devolos <utk27032005@gmail.com>
2023-08-08 09:32:31 +0530

    Commit Message: 	 - Update import statements and exports for the 'simple_sizer_web' package. Commit Description:  - This MR adds new import statements and export statements for the 'simple_sizer_web' package, including imports for different folders and extensions, and removes the outdated import statement.

--- lib/simple_sizer_web.dart
+++ lib/simple_sizer_web.dart
@@ -1,7 +1,36 @@
-///'simple_sizer_web' is the andother dependency of this package and is usually imported when dealing with flutter web.
+///'simple_sizer_web' is the andother core dependency of this package and is usually imported when dealing with flutter web.
 ///It includes `clampInRange` methods ,`reactive widgets`, and `size_config_web`.
 ///mainly used in [Flutter Web].
 library simple_sizer_web;
+
+//From Folder creators
+export 'src/exports/creators/text_creator.dart';
+export 'src/exports/creators/decoration_creator.dart';
+
+//From Folder enums
+export 'src/exports/enums/text_align_enum.dart';
+export 'src/exports/enums/align_cntr_enum.dart';
+
+//From Folder flutter-widgets
+export 'src/exports/extensions/flutter-widgets/build_ctx.dart';
+export 'src/exports/extensions/flutter-widgets/align_cntr_ext.dart';
+export 'src/exports/extensions/flutter-widgets/expanded_and_flexible_ext.dart';
+export 'src/exports/extensions/flutter-widgets/fractionally_box.dart';
+export 'src/exports/extensions/flutter-widgets/padding_ext.dart';
+export 'src/exports/extensions/flutter-widgets/aspectratio_ext.dart';
+export 'src/exports/extensions/flutter-widgets/sizedbox_ext.dart';
+export 'src/exports/extensions/flutter-widgets/fitted_box_ext.dart';
+export 'src/exports/extensions/flutter-widgets/limitedbox_ext.dart';
+export 'src/exports/extensions/flutter-widgets/stba_ext.dart';
+export 'src/exports/extensions/flutter-widgets/text_ext.dart';
+
+//From Folder data-types
+export 'src/exports/extensions/primitive-types/bool_ext.dart';
+export 'src/exports/extensions/primitive-types/string_ext.dart';
+export 'src/exports/extensions/primitive-types/int_ext.dart';
+
+//From Folder helper_methods
+export 'src/exports/helper_methods/border_rad_helper.dart';
 
 //Clamp dimensions
 export 'src/exports/helper_methods/clamp_dimensions_helper.dart';
