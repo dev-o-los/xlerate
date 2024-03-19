4cef76c44ea0ed6027fc6a1ff1dec6d39fa38597
devolos <utk27032005@gmail.com>
2024-03-19 08:06:06 +0530

    Commit Message: 	 - Refactor onTap and onTapInkWell functions in Dart for improved readability and documentation. Commit Description:  - This MR refactors the onTap and onTapInkWell functions in Dart to improve readability and documentation, adding detailed comments and updating the function signatures.

--- lib/src/exports/extensions/flutter-widgets/ontap_ext.dart
+++ lib/src/exports/extensions/flutter-widgets/ontap_ext.dart
@@ -1,9 +1,23 @@
 part of '../flutter_widget_ext.dart';
 
 extension OnTapExt on Widget {
-  GestureDetector onTap(VoidCallback action) =>
-      GestureDetector(onTap: action, child: this);
+  /// The `onTap` function in Dart returns a `GestureDetector` widget with the specified `callback`
+  /// callback for the onTap event.
+  ///
+  /// Args:
+  ///   callback (VoidCallback): The `callback` parameter is a function that will be called when the
+  /// `GestureDetector` is tapped. It is of type `VoidCallback`, which is a function that takes no
+  /// arguments and returns void.
+  GestureDetector onTap(VoidCallback callback) =>
+      GestureDetector(onTap: callback, child: this);
 
-  InkWell onTapInkWell(VoidCallback action) =>
-      InkWell(onTap: action, child: this);
+  /// The function `onTapInkWell` returns an `InkWell` widget with the specified `onTap` callback and
+  /// child widget.
+  ///
+  /// Args:
+  ///   callback (VoidCallback): The `callback` parameter in the `onTapInkWell` function is a `VoidCallback`
+  /// type, which is a function that takes no arguments and returns void. It is intended to be the
+  /// callback that is triggered when the `InkWell` widget is tapped.
+  InkWell onTapInkWell(VoidCallback callback) =>
+      InkWell(onTap: callback, child: this);
 }
